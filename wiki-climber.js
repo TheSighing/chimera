532abb86d1d9fd931a9502cbdba37789a3059f17
TheSighing <jgodfrey@DIT414D703.ucx.ucr.edu>
2015-06-18 09:53:13 -0700

    dbc

---
+++ wiki-climber.js
@@ -0,0 +1,19 @@
+var app = require('')(),
+fs = require('fs'),
+//replace with uri-request
+fs = require('request'),
+//replace with the usage of zero mq to communicate to python and use beautiful soup
+fs = require('cheerio');
+
+app.get('/climb:title', function(req, res){
+    //add a check to see if this is a valid title
+    var url = 'http://en.wikipedia.org/?title=' + req.params.title;
+
+
+})
+
+app.listen('8081')
+
+console.log('Base of the cliff is at port 8081');
+
+exports = module.exports = app;
