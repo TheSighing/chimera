aa0ff389aabe93ed9ac931685a62324852b91761
Jared Godfrey <jared.yusef@gmail.com>
2015-06-29 21:07:28 -0700

    dbc

--- climber.py
+++ climber.py
@@ -3,6 +3,12 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
+class Bubble():
+    def __init__(self, context, sub_context, text):
+        self.context =  context
+        self.sub_context = sub_context
+        self.text = text
+
 class Climber():
     def climb(self, topic):
         url = 'http://en.wikipedia.org/?title=%s' % topic
@@ -10,15 +16,28 @@ class Climber():
         soup = BeautifulSoup(r.text)
 
         paragraphs = [ p.get_text() for p in soup.find_all('p') ]
-        links = [ a.get('href') for a in soup.select('div#mw-content-text a') ]
-        images = soup.find_all('img')
-        
-        wiki = {}
-        wiki['topic'] = paragraphs
-        wiki['related'] = links
+        contexts = soup.find_all('h2#mw-headline')
+        sub_contexts = soup.find_all('h3#mw-headline')
+
+        wiki = []
+
+        for s in sub_contexts:
+            print(s.previous_sibling)
+            bubble = Bubble(c, s)
+            wiki.append(bubble)
 
         return wiki
 
+    def climb_images():
+        images = soup.find_all('img')
+        #wiki['related'] = links
+
+        return "images"
+
+    def climb_links():
+        links = [ a.get('href') for a in soup.select('div#mw-content-text a') ]
+
+        return links
 
 s = zerorpc.Server(Climber())
 s.bind("tcp://0.0.0.0:5050")
