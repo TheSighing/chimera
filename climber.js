5aac9e04e8f3841779c5698865f2af431e2eabf5
Jared Godfrey <jared.yusef@gmail.com>
2015-07-23 13:55:02 -0700

    dbc

--- climber.js
+++ climber.js
@@ -1,91 +1,122 @@
-var zerorpc = require("zerorpc"),
-async = require('async'),
-events = require('events')
-exec = require('child_process').exec;
+var zmq = require('zmq');
 
-//TODO: List
-// SWITCH THE ZERORPC OUT FOR JUST BASE ZMQ
+// socket to talk to server
+console.log("Connecting to hello world server…");
+var requester = zmq.socket('req');
 
-console.log("Starting...");
-
-// Start python server from this file as a child process to query against.
-exec('nodemon --exec \"python -v\" ./climber.py', function(err, stdout, stderr){
-  //Handle errors??
-  //Start communication and such???
-  if(err){
-    return callback(err);
+var x = 0;
+requester.on("message", function(reply) {
+  console.log("Received reply", x, ": [", reply.toString(), ']');
+  x += 1;
+  if (x === 10) {
+    requester.close();
+    process.exit(0);
   }
-
-  console.log("Initializing route...");
 });
 
-function Climber(port){
-  this.port = port;
+requester.connect("tcp://localhost:5555");
+
+for (var i = 0; i < 10; i++) {
+  console.log("Sending request", i, '…');
+  requester.send("Hello");
 }
 
-Climber.prototype = {
-  climb : function(topic, callback){
-    var e = new events.EventEmitter();
-    var client = new zerorpc.Client();
-    client.connect('tcp://127.0.0.1:' + this.port);
+process.on('SIGINT', function() {
+  requester.close();
+});
 
-    client.invoke("climb", topic, function(err, content, more){
-      console.log("climb climb climb");
-      if(err){
-        return callback(err, null);
-      }
 
-      if(!more){
-        client.close();
-        console.log("Returning data climb...");
-        return callback(null, content);
-      }
-      else{
-        //console.log(content);
-        content += content + " "
-      }
-    });
-  },
-  climb_images : function(callback){
-    var e = new events.EventEmitter();
-    var client = new zerorpc.Client();
-    client.connect('tcp://127.0.0.1:' + this.port);
 
-    client.invoke("climb_images", function(err, content, more){
-      if(err){
-        callback(err, null);
-      }
 
-      if(!more){
-        client.close();
-        console.log("Returning data climb images...");
-        return callback(null, content);
-      }
-      else{
-        content += content + " "
-      }
-    });
-  },
-  climb_links : function(callback){
-    var e = new events.EventEmitter();
-    var client = new zerorpc.Client();
-    client.connect('tcp://127.0.0.1:' + this.port);
 
-    client.invoke("climb_links", function(err, content, more){
-      if(err){
-        return callback(err, null);
-      }
 
-      if(!more){
-        client.close();
-        console.log("Returning data climb links...");
-        return callback(null, content);
-      }
-      else{
-        content += content + " "
-      }
-    });
-  }
-}
-
-exports = module.exports = Climber;
+// var zmq = require("zmq"),
+// // async = require('async'),
+// // // events = require('events')
+// exec = require('child_process').exec;
+// //TODO: List
+// // SWITCH THE ZERORPC OUT FOR JUST BASE ZMQ
+//
+// console.log("Starting...");
+//
+// // Start python server from this file as a child process to query against.
+// exec('nodemon --exec \"python -v\" ./climber.py', function(err, stdout, stderr){
+//   //Handle errors??
+//   //Start communication and such???
+//   if(err){
+//     return callback(err);
+//   }
+//
+//   console.log("Initializing route...");
+// });
+//
+// function Climber(port){
+//   this.port = port;
+// }
+//
+// Climber.prototype = {
+//   climb : function(topic, callback){
+//     var e = new events.EventEmitter();
+//     var client = new zerorpc.Client();
+//     client.connect('tcp://127.0.0.1:' + this.port);
+//
+//     client.invoke("climb", topic, function(err, content, more){
+//       console.log("climb climb climb");
+//       if(err){
+//         return callback(err, null);
+//       }
+//
+//       if(!more){
+//         client.close();
+//         console.log("Returning data climb...");
+//         return callback(null, content);
+//       }
+//       else{
+//         //console.log(content);
+//         content += content + " "
+//       }
+//     });
+//   },
+//   climb_images : function(callback){
+//     var e = new events.EventEmitter();
+//     var client = new zerorpc.Client();
+//     client.connect('tcp://127.0.0.1:' + this.port);
+//
+//     client.invoke("climb_images", function(err, content, more){
+//       if(err){
+//         callback(err, null);
+//       }
+//
+//       if(!more){
+//         client.close();
+//         console.log("Returning data climb images...");
+//         return callback(null, content);
+//       }
+//       else{
+//         content += content + " "
+//       }
+//     });
+//   },
+//   climb_links : function(callback){
+//     var e = new events.EventEmitter();
+//     var client = new zerorpc.Client();
+//     client.connect('tcp://127.0.0.1:' + this.port);
+//
+//     client.invoke("climb_links", function(err, content, more){
+//       if(err){
+//         return callback(err, null);
+//       }
+//
+//       if(!more){
+//         client.close();
+//         console.log("Returning data climb links...");
+//         return callback(null, content);
+//       }
+//       else{
+//         content += content + " "
+//       }
+//     });
+//   }
+// }
+//
+// exports = module.exports = Climber;
--- climber.py
+++ climber.py
@@ -1,27 +1,9 @@
-import zerorpc
-import requests
-import json
-from bs4 import BeautifulSoup
+import time
+import zmq
 
-#TODO: List
-# Parse and clean the text to remove the reference numbers
-# get images from wiki and apply context to them
-# def see_also() => makes a whole set of related thhings to the topic chosen
-# def chossy() => parse disambiguation pages can be called when the page reached durign climb or
-# any given method in the class and it hits a "chossy page" one that cannot be parsed in this custiomary
-# method ie a disambiguation page or otherwise
-# def flash() => grab directly a section of the overall page when supplied a set of context levels and/or
-# a bit of text that it can match
-# climb links should build based on a depth choice and and builds graph of links to help determine later searches
-# add comments to this
-# bolts should also allow for optional images.
-# climb should have options (object) passed in to allow it to include images in route or to include graph of links with given
-# level of depth
-
-#TODO: Notes
-# proven method to send object to the otherside
-# wiki_parsed.append({ "Text" :  bolt.text , "Contexts: " : bolt.contexts })
-# re.compile => a way to check for a specific string match
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
 
 class Bolt():
     def __init__(self, text):
@@ -48,9 +30,9 @@ class Bolt():
 
         return temp
 
-class Climber(object):
+class Climber():
     # Constructs route of entire wiki page based on text.
-    @zerorpc.stream
+    #@zerorpc.stream
     def climb(self, topic):
         self.topic = topic;
 
@@ -110,6 +92,141 @@ class Climber(object):
 
         return json.dumps(links, indent=4)
 
-s = zerorpc.Server(Climber())
-s.bind("tcp://0.0.0.0:5050")
-s.run()
+while True:
+    #  Wait for next request from client
+    topic = socket.recv()
+
+
+    climb(topic)
+
+    #  Do some 'work'
+    time.sleep(1)
+
+    #  Send reply back to client
+    socket.send(b"World")
+
+
+
+
+
+
+
+
+
+
+
+# import zerorpc
+# import requests
+# import json
+# from bs4 import BeautifulSoup
+#
+# #TODO: List
+# # Parse and clean the text to remove the reference numbers
+# # get images from wiki and apply context to them
+# # def see_also() => makes a whole set of related thhings to the topic chosen
+# # def chossy() => parse disambiguation pages can be called when the page reached durign climb or
+# # any given method in the class and it hits a "chossy page" one that cannot be parsed in this custiomary
+# # method ie a disambiguation page or otherwise
+# # def flash() => grab directly a section of the overall page when supplied a set of context levels and/or
+# # a bit of text that it can match
+# # climb links should build based on a depth choice and and builds graph of links to help determine later searches
+# # add comments to this
+# # bolts should also allow for optional images.
+# # climb should have options (object) passed in to allow it to include images in route or to include graph of links with given
+# # level of depth
+#
+# #TODO: Notes
+# # proven method to send object to the otherside
+# # wiki_parsed.append({ "Text" :  bolt.text , "Contexts: " : bolt.contexts })
+# # re.compile => a way to check for a specific string match
+#
+# class Bolt():
+#     def __init__(self, text):
+#         self.contexts =  {}
+#         self.text = text
+#
+#     # Add context to bolt.
+#     def belay(self, context, level=None):
+#         if(not level):
+#             self.contexts = {}
+#             self.contexts["1"] = context
+#         else:
+#             self.contexts[str(level)] = context
+#
+#     # Encodes the bolt for safe transfer through zerorpc pipeline.
+#     def encode(self):
+#         return { "Text" : self.text, "Contexts" : self.contexts }
+#
+#     def __str__(self):
+#         temp = "Text: " + self.text
+#         temp += "\nContext:"
+#         for key in self.contexts:
+#             temp += "\nlvl" + key + ": " + self.contexts[key]
+#
+#         return temp
+#
+# class Climber(object):
+#     # Constructs route of entire wiki page based on text.
+#     @zerorpc.stream
+#     def climb(self, topic):
+#         self.topic = topic;
+#
+#         self.url = 'http://en.wikipedia.org/?title=%s' % self.topic
+#         self.content = requests.get(self.url)
+#         self.soup = BeautifulSoup(self.content.text)
+#
+#         wiki_parsed = []
+#
+#         #TODO:
+#         # You are creating context, subcontext, text, links => Bolt() object and loading into an Array
+#         # building structure to the wiki itself (or any large text based information page) that can be accessed
+#         # parsed and such.
+#         # later should incorporate other checks to find titles and context
+#         # this doesnt quite work how i want it to need it to have parent contexts saved
+#         # so that only the changing level changes but the other context remain thre same for each new bolt added on
+#         # a new bolt is constituted by the fact that one of the context level has chaged
+#         # proboably should make this become hadled by the class Bolt or something
+#         # Should also work with any amount of headers (headers define amounts of context)
+#         h = ["", "", "", ""]
+#         for section in self.soup.find_all(["h1", "h2", "h3", "h4", "p"]):
+#             try:
+#                 if(section.name  == "h1"):
+#                     h[0] = section.get_text()
+#                 elif(section.name  == "h2"):
+#                     h[1] = section.get_text()
+#                 elif(section.name  == "h3"):
+#                     h[2] = section.get_text()
+#                 elif(section.name  == "h4"):
+#                     h[3] = section.get_text()
+#                 elif(section.name == "p"):
+#                     # Add text to the bolt.
+#                     bolt = Bolt(section.get_text())
+#                     bolt.belay(h[0], 1)
+#                     bolt.belay(h[1], 2)
+#                     bolt.belay(h[2], 3)
+#                     bolt.belay(h[3], 4)
+#                     wiki_parsed.append(bolt.encode())
+#                 else:
+#                     continue
+#                 pass
+#             except Exception as e:
+#                 continue
+#
+#         return json.dumps(wiki_parsed, indent=4)
+#
+#     # Extracts images and their context attached/explanation.
+#     def climb_images(self):
+#         images = self.soup.find_all('img')
+#         print images
+#
+#         return "images"
+#
+#     # Builds map of links with given search depth option as parameter.
+#     def climb_links(self):
+#         links = [ a.get('href') for a in self.soup.select('div#mw-content-text a') ]
+#
+#         return json.dumps(links, indent=4)
+#
+# s = zerorpc.Server(Climber())
+# s.bind("tcp://0.0.0.0:5050")
+# s.run()
