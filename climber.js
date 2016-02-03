ad8d7144cf01155d59d22b40406ce1de6a3829c0
thesighing <jared.yusef@gmail.com>
2016-02-03 12:21:26 -0800

    So it begins.

--- climber.js
+++ climber.js
@@ -1,23 +1,21 @@
+//TODO: List
 var zerorpc = require("zerorpc"),
 async = require('async'),
 events = require('events')
 exec = require('child_process').exec;
 
-//TODO: List
-// or some other way to have the python zerorpc server available
-
 console.log("Starting...");
 
 // Start python server from this file as a child process to query against.
-//exec('nodemon --exec \"python -v\" ./climber.py', function(err, stdout, stderr){
-//  //Handle errors??
-//  //Start communication and such???
-//  if(err){
-//    return callback(err);
-//  }
-//
-//  console.log("Initializing route...");
-//});
+exec('nodemon --exec \"python -v\" ./climber.py', function(err, stdout, stderr){
+  //Handle errors??
+  //Start communication and such???
+  if(err){
+    return callback(err);
+  }
+
+  console.log("Initializing route...");
+});
 
 function Climber(port){
   this.port = port;
--- climber.py
+++ climber.py
@@ -8,16 +8,46 @@ from bs4 import BeautifulSoup
 # Parse and clean the text to remove the reference numbers
 # get images from wiki and apply context to them
 # def see_also() => makes a whole set of related thhings to the topic chosen
-# def chossy() => parse disambiguation pages can be called when the page reached durign climb or
-# any given method in the class and it hits a "chossy page" one that cannot be parsed in this custiomary
+# def chossy() => parse disambiguation pages can be called
+# when the page reached durign climb or
+# any given method in the class and it hits a "chossy page"
+# one that cannot be parsed in this custiomary
 # method ie a disambiguation page or otherwise
-# def flash() => grab directly a section of the overall page when supplied a set of context levels and/or
+# def flash() => grab directly a section of the overall page when supplied
+#a set of context levels and/or
 # a bit of text that it can match
-# climb links should build based on a depth choice and and builds graph of links to help determine later searches
+# climb links should build based on a depth choice and and builds graph of links
+# to help determine later searches
 # add comments to this
 # bolts should also allow for optional images.
-# climb should have options (object) passed in to allow it to include images in route or to include graph of links with given
+# climb should have options (object) passed in to allow it to include images
+# in route or to include graph of links with given
 # level of depth
+
+#TODO:
+# You are creating context, subcontext, text, links => Bolt() object
+# and loading into an Array
+# building structure to the wiki itself
+# (or any large text based information page) that can be accessed
+# parsed and such.
+# later should incorporate other checks to find titles and context
+# this doesnt quite work how i want it to need it to have parent contexts saved
+# so that only the changing level changes but the other context remain the
+# same for each new bolt added on
+# a new bolt is constituted by the fact that one of the context level has chaged
+# proboably should make this become hadled by the class Bolt or something
+# Should also work with any amount of headers
+#(headers define amounts of context)
+# create overall function that sanatizes the strings for printing them and for
+# putting them through the summarizer removing the (dirt from the wiki page
+# stuff like the square bracket number stuff) this should be the case for the
+# normal and the beginnign stages of this project but later may provive a
+# detailed mapping of the web of wiki pages such that better summaries can be
+# made from other pages and included int eh returned product for the main page
+# being queried.
+#fix the h1 - ?? checks so they are extensible rt=ather than hard coded this so it matches the h# set up and loops to
+#decide on depth or just inputs the number found
+#as the hash for the entry
 
 #TODO: Notes
 # proven method to send object to the otherside
@@ -63,16 +93,6 @@ class Climber(object):
 
         wiki_parsed = []
 
-        #TODO:
-        # You are creating context, subcontext, text, links => Bolt() object and loading into an Array
-        # building structure to the wiki itself (or any large text based information page) that can be accessed
-        # parsed and such.
-        # later should incorporate other checks to find titles and context
-        # this doesnt quite work how i want it to need it to have parent contexts saved
-        # so that only the changing level changes but the other context remain thre same for each new bolt added on
-        # a new bolt is constituted by the fact that one of the context level has chaged
-        # proboably should make this become hadled by the class Bolt or something
-        # Should also work with any amount of headers (headers define amounts of context)
         check = self.soup.find_all(id="disambigbox")
 
         if(not len(check)):
@@ -80,32 +100,41 @@ class Climber(object):
             h = ["", "", "", ""]
             for section in self.soup.find_all(["h1", "h2", "h3", "h4", "p"]):
                 try:
-                    #fix this so it matches the h# set up and loops to decide on depth or just inputs the number found as the hash for the entry
                     if(section.name  == "h1"):
-                        h[0] = section.get_text()
+                        text = section.get_text()
+                        if(text != "Contents" and text != ""):
+                            h[0] = text
                     elif(section.name  == "h2"):
-                        h[1] = section.get_text()
-                        h[2] = ""
-                        h[3] = ""
+                        text = section.get_text()
+                        if(text != "Contents" and text != ""):
+                            h[1] = text
+                            h[2] = ""
+                            h[3] = ""
                     elif(section.name  == "h3"):
-                        h[2] = section.get_text()
-                        h[3] = ""
+                        text = section.get_text()
+                        if(text != "Contents" and text != ""):
+                            h[2] = text
+                            h[3] = ""
                     elif(section.name  == "h4"):
-                        h[3] = section.get_text()
+                        text = section.get_text()
+                        if(text != "Contents" and text != ""):
+                            h[3] = text
                     elif(section.name == "p"):
                         # Add text to the bolt.
                         string = section.get_text()
-                        string = re.sub(r"\[\d+\]", "", string)
-                        bolt = Bolt(string)
-                        bolt.belay(h[0], 1)
-                        bolt.belay(h[1], 2)
-                        bolt.belay(h[2], 3)
-                        bolt.belay(h[3], 4)
-                        wiki_parsed.append(bolt.encode())
+                        if(string != ""):
+                            string = re.sub(r"\[\d+\]", "", string)
+                            bolt = Bolt(string)
+                            bolt.belay(h[0], 1)
+                            bolt.belay(h[1], 2)
+                            bolt.belay(h[2], 3)
+                            bolt.belay(h[3], 4)
+                            wiki_parsed.append(bolt.encode())
                     else:
                         continue
                     pass
                 except Exception as e:
+                    print e
                     continue
         else:
             chossy()
