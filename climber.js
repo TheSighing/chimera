e807140e3d6361030b54bcb63777f4ca69dfcf94
thesighing <jared.yusef@gmail.com>
2016-02-08 14:50:26 -0800

    dbc

--- climber.js
+++ climber.js
@@ -3,17 +3,6 @@ var zerorpc = require("zerorpc"),
 async = require('async'),
 events = require('events');
 
-
-//exec('nodemon --exec \"python -v\" ./climber.py', function(err, stdout, stderr){
-//exec('\"python -v\" ./climber.py', function(err, stdout, stderr){
-//    //Handle errors??
-//    //Start communication and such???
-//    //ensure this closes when done using it nad other debugging
-//    if(err){
-//        return callback(err);
-//    }
-//});
-
 function Climber(port){
     this.port = port;
 }
@@ -21,38 +10,22 @@ function Climber(port){
 Climber.prototype = {
     climb : function(topic, callback){
         var e = new events.EventEmitter();
-        // Start python server from this file as a child process to query against.
-        console.log("I did not hit her!");
-        //var childProcess = require('child_process');
-        //childProcess.exec('python ./climber.py', function(error, stdout, stderr){
-        //    if(error){
-        //        console.log(error.stack);
-        //             console.log('Error code: '+error.code);
-        //                  console.log('Signal received: '+error.signal);
-        //    }
+        var client = new zerorpc.Client();
+        client.connect('tcp://127.0.0.1:' + this.port);
 
-        //    console.log("its bullshit.");
-        //    console.log('Child Process STDOUT: '+stdout);
-        //       console.log('Child Process STDERR: '+stderr);
-        //});
-            var client = new zerorpc.Client();
-            client.connect('tcp://127.0.0.1:' + this.port);
-            console.log("Shut up Lisa.");
+        client.invoke("climb", topic, function(err, content, more){
+            if(err){
+                return callback(err, null);
+            }
 
-            client.invoke("climb", topic, function(err, content, more){
-                if(err){
-                    return callback(err, null);
-                }
-
-                if(!more){
-                    client.close();
-                    return callback(null, content);
-                }
-                else{
-                    content += content + " ";
-                }
-            });
-
+            if(!more){
+                client.close();
+                return callback(null, content);
+            }
+            else{
+                content += content + " ";
+            }
+        });
     },
 
     climb_images : function(callback){
