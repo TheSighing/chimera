78a3bc8551a9ab122897e4d3a044ebb504da8c34
thesighing <jared.yusef@gmail.com>
2016-02-08 10:27:22 -0800

    dbc

--- climber.js
+++ climber.js
@@ -3,6 +3,7 @@ var zerorpc = require("zerorpc"),
 async = require('async'),
 events = require('events');
 
+
 //exec('nodemon --exec \"python -v\" ./climber.py', function(err, stdout, stderr){
 //exec('\"python -v\" ./climber.py', function(err, stdout, stderr){
 //    //Handle errors??
@@ -21,16 +22,18 @@ Climber.prototype = {
     climb : function(topic, callback){
         var e = new events.EventEmitter();
         // Start python server from this file as a child process to query against.
-            console.log("your terring me apart lisa.");
-        var child_process = require('child_process');
-        child_process.execFile('python', ["./climber.py"], function(err, stdout, stderr){
+        console.log("your tearing me apart lisa.");
+        var PythonShell = require('python-shell');
+        PythonShell.run('climber.py', function(err){
             if(err){
                 throw err;
             }
 
             console.log("Shut up.");
+        });
             var client = new zerorpc.Client();
             client.connect('tcp://127.0.0.1:' + this.port);
+            console.log("Shut up.");
 
             client.invoke("climb", topic, function(err, content, more){
                 if(err){
@@ -45,7 +48,6 @@ Climber.prototype = {
                     content += content + " ";
                 }
             });
-        });
 
     },
 
